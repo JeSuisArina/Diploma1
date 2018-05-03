8caaab095c01eab4d29c903255f99ecd16546342
Arina <leonovaap@gmail.com>
2018-05-03 23:03:14 +0300

    create подразделения, немного стилей

---
+++ InternalAuditSystem/InternalAuditSystem/Controllers/NewsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using InternalAuditSystem.Models.EntityModels;
+
+namespace InternalAuditSystem.Controllers
+{
+    public class NewsController : Controller
+    {
+        private AuditContext db = new AuditContext();
+
+        // GET: News
+        public ActionResult Index()
+        {
+            return View(db.NewsView.ToList());
+        }
+
+        // GET: News/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NewsView newsView = db.NewsView.Find(id);
+            if (newsView == null)
+            {
+                return HttpNotFound();
+            }
+            return View(newsView);
+        }
+
+        // GET: News/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: News/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "NewsContent,NewsDateTime,UserLastName,UserName,NewsId,NewsTitle,UserMiddleName")] NewsView newsView)
+        {
+            if (ModelState.IsValid)
+            {
+                db.NewsView.Add(newsView);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(newsView);
+        }
+
+        // GET: News/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NewsView newsView = db.NewsView.Find(id);
+            if (newsView == null)
+            {
+                return HttpNotFound();
+            }
+            return View(newsView);
+        }
+
+        // POST: News/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "NewsContent,NewsDateTime,UserLastName,UserName,NewsId,NewsTitle,UserMiddleName")] NewsView newsView)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(newsView).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(newsView);
+        }
+
+        // GET: News/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NewsView newsView = db.NewsView.Find(id);
+            if (newsView == null)
+            {
+                return HttpNotFound();
+            }
+            return View(newsView);
+        }
+
+        // POST: News/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            NewsView newsView = db.NewsView.Find(id);
+            db.NewsView.Remove(newsView);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
